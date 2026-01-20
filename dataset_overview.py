97710998045d96df5385d4cbcad1c6273343f514
Simon Wolffhardt <s.wolffhardt@protonmail.com>
2026-01-20 12:06:36 +0100

    Remove header in data source

--- dataset_overview.py
+++ dataset_overview.py
@@ -18,12 +18,10 @@ This section provides an overview of the diabetic dataset used for analysis.
 """)
 
 with st.expander("Dataset Source"):
-    st.markdown("""
-    ### Data Source
-    
+    st.markdown("""    
     **Dataset Name:** Diabetes 130-US Hospitals for Years 1999-2008
     
-    **Source:** https://archive.ics.uci.edu/dataset/296/diabetes+130-us+hospitals+for+years+1999-2008
+    **Link:** https://archive.ics.uci.edu/dataset/296/diabetes+130-us+hospitals+for+years+1999-2008
     
     **Citation:** 
     ```
